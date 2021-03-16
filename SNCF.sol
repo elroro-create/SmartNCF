1e2a17c6a5d5499855f7a6651bb1fe741ea27439
elroro-create <78745068+elroro-create@users.noreply.github.com>
2021-03-16 15:34:37 +0100

    Create SNCF.sol

---
+++ SNCF.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Sncf {
+    
+    address contractOwner;
+
+    struct Voyageur {
+        string Nom;
+        string Prenom;
+        string RefTrain; //format de RefTrain : DEPARTARRIVEECLASSEVOITURENUMERO (exemple MARSEILLEPARIS1120)
+        address ClinsAddress;
+
+    }
+
+    struct EtatTrain {
+        string depart;
+        string arrivee;
+        uint retard;
+        bool etat;
+    }
+
+    Voyageur[] public billet;
+
+    mapping(address => Voyageur) private voyageur;
+
+    function sncf() public {
+        contractOwner = msg.sender;
+    }
+
+    function 
+
+    function getVoyageur() public returns (uint) {
+        return billet.length;
+    }
+
+    function addNewVoyageur(string memory _nom, string memory _prenom, string memory _RefTrain, address _ClinsAddress) public returns(bool success) {
+        Voyageur memory NewVoyageur;
+        NewVoyageur.Nom = _nom;
+        NewVoyageur.Prenom = _prenom;
+        NewVoyageur.RefTrain = _RefTrain;
+        NewVoyageur.ClinsAddress = _ClinsAddress;
+        billet.push(NewVoyageur);
+        return true;
+
+    }
+
+
+
+
+}
